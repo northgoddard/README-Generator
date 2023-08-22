f035c1748941acd1b2ff79340c12034cdf6b7561
northgoddard <129123028+northgoddard@users.noreply.github.com>
2023-08-22 10:50:41 -0700

    License Section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,13 +18,20 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
+
+  if (license != "None") {
+    licenseSection += "## Liscense\n"
+    licenseSection += "Please see " + renderLicenseLink(license) + " to get detailed information for this license\n";
+  }
+
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  const sections = ["Description", "Installation", "Usage", "Contributing", "Tests", "License", "Questions"];
 }
 
 module.exports = generateMarkdown;
